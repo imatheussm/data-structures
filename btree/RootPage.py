33889ca1a382fc7643f37ac9851f77d15a8efcbc
Igor Matheus Souza Moreira <imatheus.sm@gmail.com>
2019-09-09 12:42:32 -0300

    Formating updated
    Using black formatter.

--- btree/RootPage.py
+++ btree/RootPage.py
@@ -1,23 +1,32 @@
 from btree.constants import *
 
+
 class RootPage:
     """The root of the B-Tree."""
-    
+
     def __init__(self, min_num_keys, *args):
-        self.min_num_keys = min_num_keys
+        #self.depth = 0
+        #self.height = 1
+        self.keys = []
+        self.level = self.depth + 1
         self.max_num_keys = 2 * min_num_keys
-        self.num_keys     = 0
-        self.keys         = []
-        
+        self.min_num_keys = min_num_keys
+        self.num_keys = 0
+
         for arg in args:
             self.insert(arg)
-            
+
     def insert(self, element):
         """Inserts a number in the B-Tree."""
-        
+
         if type(element) in SUPPORTED_LIST_TYPES:
-            for item in element: self.insert(item)
+            for item in element:
+                self.insert(item)
         elif type(element) in SUPPORTED_TYPES:
             self.keys.append(element)
         else:
-            raise TypeError("This type is not supported. Type of the element: {}".format(type(element)))
+            raise TypeError(
+                "This type is not supported. Type of the element: {}".format(
+                    type(element)
+                )
+            )
