407904a4d860fafe7ce9ba22ab86d92759c2a2af
Igor Matheus Souza Moreira <imatheus.sm@gmail.com>
2019-09-09 10:15:28 -0300

    Create initial RootPage structure
    This structure shall be inherited by Page and LeafPage classes.

--- b-tree/RootPage.py
+++ b-tree/RootPage.py
@@ -1,8 +1,23 @@
+from b-tree.constants import *
+
 class RootPage:
-    """The B-Tree itself."""
+    """The root of the B-Tree."""
     
-    def __init__(self, min_keys, *args):
-        self.min_keys, self.max_keys = min_keys, 2 * min_keys
+    def __init__(self, min_num_keys, *args):
+        self.min_num_keys = min_num_keys
+        self.max_num_keys = 2 * min_num_keys
+        self.num_keys     = 0
+        self.keys         = []
         
         for arg in args:
+            self.insert(arg)
             
+    def insert(self, element):
+        """Inserts a number in the B-Tree."""
+        
+        if type(element) in supported_list_types:
+            for item in element: self.insert(item)
+        elif type(element) in supported_types:
+            self.keys.append(element)
+        else:
+            raise TypeError("This type is not supported. Type of the element: {}".format(type(element)))
