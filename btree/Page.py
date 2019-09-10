3eb256a6ac2ec27a63d5058960a65f4202e56c14
Igor Matheus Souza Moreira <imatheus.sm@gmail.com>
2019-09-10 13:49:30 -0300

    Constructor parameters updated
    Optional parameters will not be loaded if set to None.

--- btree/Page.py
+++ btree/Page.py
@@ -2,25 +2,38 @@ import btree.helper as helper
 from btree.RootPage import *
 from btree.DegreeOverflow import *
 
+
 class Page:
     """A Page of the B-Tree.
     
     This page is inherited by RootPage and LeafPage classes.
     """
-    
-    def __init__(self, min_num_keys, parent_tree=None, *args):
+
+    def __init__(
+        self,
+        min_num_keys,
+        parent_tree=None,
+        parent_page=None,
+        descendent_pages=None,
+        *args,
+    ):
         # self.depth = 0
         # self.height = 1
         # self.level = self.depth + 1
+        if descendent_pages and type(descendent_pages) == list:
+            self.descendent_pages = descendent_pages
         self.keys = []
         self.max_num_keys = 2 * min_num_keys
         self.min_num_keys = min_num_keys
         self.num_keys = 0
-        self.parent_tree = parent_tree
-        
+        if parent_tree:
+            self.parent_page = parent_page
+        if parent_page:
+            self.parent_tree = parent_tree
+
         for arg in args:
             self.insert(arg)
-            
+
     def insert(self, element):
         """Inserts a number in the B-Tree."""
 
@@ -40,6 +53,6 @@ class Page:
                 )
             )
         self.num_keys += 1
-        
+
         if len(self.keys) > self.max_num_keys:
             raise DegreeOverflow(self)
