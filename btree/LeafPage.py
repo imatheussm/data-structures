5e61974244c7bd0eaac3dcf923e64aacea2abb2f
Igor Matheus Souza Moreira <imatheus.sm@gmail.com>
2019-09-10 13:46:03 -0300

    LeafPage class created
    Just a basic sketch.

--- btree/LeafPage.py
+++ btree/LeafPage.py
@@ -1,1 +1,19 @@
 from btree.Page import *
+
+
+class LeafPage(Page):
+    """The root of the B-Tree."""
+
+    def __init__(
+        self,
+        min_num_keys,
+        parent_tree=None,
+        parent_page=None,
+        descendent_pages=None,
+        *args,
+    ):
+        super().__init__(
+            min_num_keys, parent_tree, parent_page, descendent_pages, *args
+        )
+        del self.descendent_pages
+
