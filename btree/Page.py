cc0b25856d015d1ecb22965d1ee7d92c0f4a89d9
Igor Matheus Souza Moreira <imatheus.sm@gmail.com>
2019-09-10 10:21:38 -0300

    Refactoring and hierarchical changes
    Now, RootPage and LeafPage classes shall both inherit from Page. The method .insert() is now under Page.

--- btree/Page.py
+++ btree/Page.py
@@ -1,10 +1,45 @@
+import btree.helper as helper
 from btree.RootPage import *
+from btree.DegreeOverflow import *
 
-class Page(RootPage):
+class Page:
     """A Page of the B-Tree.
     
     This page is inherited by RootPage and LeafPage classes.
     """
     
-    def __init__(self):
-        pass
+    def __init__(self, min_num_keys, parent_tree=None, *args):
+        # self.depth = 0
+        # self.height = 1
+        # self.level = self.depth + 1
+        self.keys = []
+        self.max_num_keys = 2 * min_num_keys
+        self.min_num_keys = min_num_keys
+        self.num_keys = 0
+        self.parent_tree = parent_tree
+        
+        for arg in args:
+            self.insert(arg)
+            
+    def insert(self, element):
+        """Inserts a number in the B-Tree."""
+
+        if type(element) in SUPPORTED_LIST_TYPES:
+            for item in element:
+                self.insert(item)
+            return
+        elif type(element) in SUPPORTED_TYPES:
+            if len(self.keys) == 0:
+                self.keys.append(element)
+            else:
+                helper.insert_crescent(element, self.keys)
+        else:
+            raise TypeError(
+                "This type is not supported. Type of the element: {}".format(
+                    type(element)
+                )
+            )
+        self.num_keys += 1
+        
+        if len(self.keys) > self.max_num_keys:
+            raise DegreeOverflow(self)
--- btree/RootPage.py
+++ btree/RootPage.py
@@ -1,39 +1,12 @@
 from btree.constants import *
+from btree.Page import *
 from time import sleep
-import btree.helper as helper
 
 
-class RootPage:
+class RootPage(Page):
     """The root of the B-Tree."""
 
     def __init__(self, min_num_keys, parent_tree=None, *args):
-        # self.depth = 0
-        # self.height = 1
-        # self.level = self.depth + 1
-        self.keys = []
-        self.max_num_keys = 2 * min_num_keys
-        self.min_num_keys = min_num_keys
-        self.num_keys = 0
-        self.parent_tree = parent_tree
-        self.pointers = []
-
-        for arg in args:
-            self.insert(arg)
-
-    def insert(self, element):
-        """Inserts a number in the B-Tree."""
-
-        if type(element) in SUPPORTED_LIST_TYPES:
-            for item in element:
-                self.insert(item)
-        elif type(element) in SUPPORTED_TYPES:
-            if len(self.keys) == 0:
-                self.keys.append(element)
-            else:
-                helper.insert_crescent(element, self.keys)
-        else:
-            raise TypeError(
-                "This type is not supported. Type of the element: {}".format(
-                    type(element)
-                )
-            )
+        super().__init__(min_num_keys, parent_tree, *args)
+        
+        self.next_pages = []
