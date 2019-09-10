f97f5224c104ac5482f94551863a5ec4f7bfc5d7
Igor Matheus Souza Moreira <imatheus.sm@gmail.com>
2019-09-10 10:22:06 -0300

    Default message set
    Now, there's a default message. Further improvements are to be made.

--- btree/DegreeOverFlow.py
+++ btree/DegreeOverFlow.py
@@ -1,7 +1,9 @@
 class DegreeOverflow(OverflowError):
     """Error that happens when a B-Tree has too many keys."""
 
-    def __init__(self, page, message):
+    default_message = "The number of elements has surpasses the maximum amount allowed. Deploying measures..."
+    
+    def __init__(self, page, message=default_message):
         super().__init__(message)
 
         # CREATE HANDLING OF OVERFLOW (MANY ITEMS IN LIST)
