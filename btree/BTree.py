320adbbca493dd0d2556df2a5eee612f9830c5fb
Igor Matheus Souza Moreira <imatheus.sm@gmail.com>
2019-09-10 09:41:56 -0300

    Doubly-linking BTree and RootPage classes
    Now, the BTree and RootPage classes are doubly-linked. My intention here is to put the verification methods in the BTree class; however, I still have to see if this is elegant or not.

--- btree/BTree.py
+++ btree/BTree.py
@@ -1,7 +1,8 @@
 from btree.RootPage import *
 
+
 class BTree:
     """The B-Tree object per se."""
-    
+
     def __init__(self, min_num_keys, *args):
-        self.root = RootPage(min_num_keys, *args)
+        self.root = RootPage(min_num_keys, self, *args)
