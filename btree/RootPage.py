b5c6f2087291859fb280e0140637bc59efecb4b7
Igor Matheus Souza Moreira <imatheus.sm@gmail.com>
2019-09-10 13:50:31 -0300

    Constructor extended and reformatted
    It meets the expectations of the Page class constructor. In addition, this code has been formatted with black.

--- btree/RootPage.py
+++ btree/RootPage.py
@@ -6,7 +6,16 @@ from time import sleep
 class RootPage(Page):
     """The root of the B-Tree."""
 
-    def __init__(self, min_num_keys, parent_tree=None, *args):
-        super().__init__(min_num_keys, parent_tree, *args)
-        
-        self.next_pages = []
+    def __init__(
+        self,
+        min_num_keys,
+        parent_tree=None,
+        parent_page=None,
+        descendent_pages=None,
+        *args
+    ):
+        super().__init__(
+            min_num_keys, parent_tree, parent_page, descendent_pages, *args
+        )
+        del self.parent_page
+
