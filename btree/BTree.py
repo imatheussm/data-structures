3a9e5ae5f6504df598f349c8272ff38df18bb8e0
Igor Matheus Souza Moreira <imatheus.sm@gmail.com>
2019-09-11 10:16:02 -0300

    .promoteRoot() moved from DegreeOverflowError
    It was creating a problem with the creation of Page objects, since it implicated in circular imports.

--- btree/BTree.py
+++ btree/BTree.py
@@ -5,4 +5,19 @@ class BTree:
     """The B-Tree object per se."""
 
     def __init__(self, min_num_keys, *args):
-        self.root = RootPage(min_num_keys, self, *args)
+        self.root = RootPage(min_num_keys, self, None, *args)
+
+    def promoteRoot(self, page):
+        middle_index = int((len(page.keys) - 1) / 2)
+        print("middle_index: {}".format(middle_index))
+        left_child = page.keys[:middle_index]
+        right_child = page.keys[middle_index + 1 :]
+        page.keys = [middle_index]
+        try:
+            if page.descendent_pages and len(page.descendent_pages) != 0:
+                print("CUIDADO COM OS PONTEIROS! TEM FILHO AQUI!")
+        except AttributeError:
+            page.descendent_pages = [
+                Page(page.min_num_keys, page.parent_tree, page, None, left_child),
+                Page(page.min_num_keys, page.parent_tree, page, None, right_child),
+            ]
