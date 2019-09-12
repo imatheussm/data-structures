179bd2b10d10f9433cd6afe85f832e52ecb895b8
Igor Matheus Souza Moreira <imatheus.sm@gmail.com>
2019-09-11 21:29:41 -0300

    .insert(), .find() created, overall improvements
    There has been quite some changes around here. First, BTree.insert() has been created. It uses the other method created, .find(), to see if the element to be inserted already exists. If it does, nothing is changed; otherwise, since the .find() method returns the page which is closest to the right place, BTree.insert() acts upon the returned page by inserting the element in it (through BTree.Page.insert() method). In case the Page.insert() method, after insertion of the item, raises an DegreeOverflowError(), the BTree.insert() method will handle it by calling .promoteRoot(), which will separate the page into two ones.
    
    There's way more to be said than it has been, but I think this will suffice. It is pertinent to say that basic testing has proven these methods to be working; however, further testing is necessary to be sure.

--- btree/BTree.py
+++ btree/BTree.py
@@ -1,23 +1,76 @@
 from btree.RootPage import *
-
+from btree.helper import *
+from btree.DegreeOverflowError import *
 
 class BTree:
     """The B-Tree object per se."""
 
     def __init__(self, min_num_keys, *args):
-        self.root = RootPage(min_num_keys, self, None, *args)
+        self.root = RootPage(min_num_keys, self)
+        self.num_keys = 0
+        
+        elements_to_insert = merge_to_list(*args)
+        for element in elements_to_insert:
+            try:
+                #self.insert(element) TO BE CREATED
+                print("[BTree.__init__()] Argument: {}".format(element))
+                self.insert(element)
+            except DegreeOverflowError:
+                pass
+        del(elements_to_insert)
+
+    def insert(self, *args):
+        for arg in args:
+            print("[BTree.insert()] Argument: {}".format(arg))
+            if type(arg) in SUPPORTED_LIST_TYPES:
+                for item in arg:
+                    self.insert(arg)
+                return
+            elif type(arg) in SUPPORTED_TYPES:
+                page_pointer = self.find(arg)
+                if page_pointer[0] == True:
+                    print("O valor {} já está na árvore.".format(arg))
+                    return
+                try:
+                    page_pointer[1].insert(arg)
+                except DegreeOverflowError as e:
+                    self.promoteRoot(e.page)
+            else:
+                raise TypeError(
+                    "This type is not supported. Type of the argumenet: {}".format(
+                        str(type(arg))
+                    )
+                )
+                
+            self.num_keys += 1
 
     def promoteRoot(self, page):
         middle_index = int((len(page.keys) - 1) / 2)
-        print("middle_index: {}".format(middle_index))
-        left_child = page.keys[:middle_index]
-        right_child = page.keys[middle_index + 1 :]
-        page.keys = [middle_index]
-        try:
-            if page.descendent_pages and len(page.descendent_pages) != 0:
-                print("CUIDADO COM OS PONTEIROS! TEM FILHO AQUI!")
-        except AttributeError:
-            page.descendent_pages = [
-                Page(page.min_num_keys, page.parent_tree, page, None, left_child),
-                Page(page.min_num_keys, page.parent_tree, page, None, right_child),
-            ]
+        left_child = page.keys[:middle_index].copy()
+        right_child = page.keys[middle_index + 1 :].copy()
+        
+        print("left_child: {}, right_child: {}".format(left_child, right_child))
+        
+        page.keys = [page.keys[middle_index]]
+        page.descendent_pages[0] = Page(
+            page.min_num_keys, page.parent_tree, page
+        )
+        for child in left_child:
+            page.descendent_pages[0].insert(child)
+        page.descendent_pages[1] = Page(
+            page.min_num_keys, page.parent_tree, page
+        )
+        for child in right_child:
+            page.descendent_pages[1].insert(child)
+
+    def find(self,element):
+        page_pointer = self.root
+        
+        while True:
+            if element in page_pointer:
+                return (True, page_pointer)
+            else:
+                new_page_pointer = page_pointer.return_pointer(element)
+                if new_page_pointer: page_pointer = new_page_pointer
+                else: 
+                    return (False, page_pointer)
