5b7d9e8843c7ddb60008c613bd1111b93084e500
Igor Matheus Souza Moreira <imatheus.sm@gmail.com>
2019-09-11 21:23:25 -0300

    Overall improvements, merge_to_list() created
    In addition to overall improvements in the previously created functions, now there is merge_to_list(). This function basically transforms any kind of list containing lists into a one-dimensional list. It is used to give greater flexibility when inserting more than one element at once.

--- btree/helper.py
+++ btree/helper.py
@@ -1,3 +1,6 @@
+from btree.constants import *
+
+
 def insert_crescent(element, array):
     """Inserts an element in a list maintaining the numbers in crescent order.
     
@@ -8,8 +11,10 @@ def insert_crescent(element, array):
     array: list
         The list into which the element shall be inserted.
     """
+    print("[helper.insert_crescent()] Element: {}. Array: {}".format(element, array))
     if len(array) == 0:
         array.append(element)
+        return
     for (index, key) in enumerate(array):
         if element > key:
             continue
@@ -22,6 +27,19 @@ def insert_crescent(element, array):
 
 def is_class(obj, class_name):
     try:
-        return obj.__module__ == "btree." + class_name and type(obj).__name__ == class_name
+        return (
+            obj.__module__ == "btree." + class_name and type(obj).__name__ == class_name
+        )
     except:
         return False
+
+
+def merge_to_list(*args):
+    final_list = []
+    for element in args:
+        if type(element) in SUPPORTED_LIST_TYPES:
+            for item in element:
+                final_list.extend(merge_to_list(item))
+        elif type(element) in SUPPORTED_TYPES:
+            final_list.append(element)
+    return final_list
