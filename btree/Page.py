68de2754fe116433805f04c442e276288f78cd91
Igor Matheus Souza Moreira <imatheus.sm@gmail.com>
2019-09-11 21:32:14 -0300

    .__contains__(), .__len__(), .__iter__(), and more were created
    Quite some methods were created here: in addition to the aforementioned ones, there's also .__getitem__(), .return_pointer() and .find(). Besides these new methods, others have been refactored, dividing responsibilities with BTree methods.
    
    It is my belief that these distribution of tasks will make further development easier. Only time will tell if all these additional structures will pay off.

--- btree/Page.py
+++ btree/Page.py
@@ -1,4 +1,4 @@
-import btree.helper as helper
+from btree.helper import *
 from btree.constants import *
 from btree.DegreeOverflowError import *
 
@@ -9,19 +9,11 @@ class Page:
     This page is inherited by RootPage and LeafPage classes.
     """
 
-    def __init__(
-        self,
-        min_num_keys,
-        parent_tree=None,
-        parent_page=None,
-        descendent_pages=None,
-        *args,
-    ):
+    def __init__(self, min_num_keys, parent_tree=None, parent_page=None):
         # self.depth = 0
         # self.height = 1
         # self.level = self.depth + 1
-        if descendent_pages and type(descendent_pages) == list:
-            self.descendent_pages = descendent_pages
+        self.descendent_pages = (min_num_keys * 2 + 1) * [None]
         self.keys = []
         self.max_num_keys = 2 * min_num_keys
         self.min_num_keys = min_num_keys
@@ -31,35 +23,35 @@ class Page:
         if parent_tree:
             self.parent_tree = parent_tree
 
-        self.insert(args)
-
-    def __contains__(self,value):
+    def __contains__(self, value):
         return value in self.keys
 
     def __repr__(self):
         return "[ " + " | ".join([(str(key)) for key in self.keys]) + " ]"
 
+    def __len__(self):
+        return len(self.keys)
+    
+    def __iter__(self):
+        return iter(self.keys)
+
+    def __getitem__(self, *args, **kwargs):
+        return self.keys.__getitem__(*args, **kwargs)
+
     def insert(self, element):
         """Inserts a number in the B-Tree."""
-
-        if type(element) in SUPPORTED_LIST_TYPES:
-            for item in element:
-                self.insert(item)
-            return
-        elif type(element) in SUPPORTED_TYPES:
-            if len(self.keys) == 0:
-                self.keys.append(element)
-            else:
-                helper.insert_crescent(element, self.keys)
-        else:
-            raise TypeError(
-                "This type is not supported. Type of the element: {}".format(
-                    type(element)
-                )
-            )
+        print("[Page.insert()] Element: {}".format(element))
+        insert_crescent(element, self.keys)
         self.num_keys += 1
+        if len(self.keys) > self.max_num_keys:
+            raise DegreeOverflowError(self)
 
-        #try:
-        #    if len(self.keys) > self.max_num_keys:
-        #        raise DegreeOverflowError(self)
-        #    else: raise NotImplementedError
+    def return_pointer(self, element):
+        for i in range(len(self)):
+            if self[i] > element:
+                return self.descendent_pages[i]
+        return self.descendent_pages[len(self)]
+    
+    def find(self, element):
+        try: return self.keys.index(element)
+        except ValueError: return None
