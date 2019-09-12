a174c48e6d5650e80690cf3cce5a1e784b22d060
Igor Matheus Souza Moreira <imatheus.sm@gmail.com>
2019-09-12 10:38:30 -0300

    Documentation added, code optimized
    In addition to a vast documentation, the code was optimized to remove repeated and unnecessary steps.

--- btree/BTree.py
+++ btree/BTree.py
@@ -7,20 +7,42 @@ class BTree:
     """The B-Tree object per se."""
 
     def __init__(self, min_num_keys, *args):
+        """The BTree class constructor.
+        
+        Parameters
+        ----------
+        self: BTree
+            A BTree object.
+        min_num_keys: int
+            The degree of the tree. This value will be applied to all Page objects and objects of Page subclasses as well.
+        *args: list
+            The elements to be inserted. Any further treatment and measures shall be taken by BTree.insert().
+        
+        Returns
+        -------
+        BTree
+            A BTree object.
+        """
         self.root = RootPage(min_num_keys, self)
         self.num_keys = 0
 
-        elements_to_insert = merge_to_list(*args)
-        for element in elements_to_insert:
-            try:
-                # self.insert(element) TO BE CREATED
-                print("[BTree.__init__()] Argument: {}".format(element))
-                self.insert(element)
-            except DegreeOverflowError:
-                pass
-        del elements_to_insert
+        print("[BTree.__init__()] *args: {}".format(*args))
+        self.insert(*args)
 
     def insert(self, *args):
+        """Inserts any amount of items into the BTree.
+        
+        Parameters
+        ----------
+        self: BTree
+            A BTree object.
+        *args: list
+            The elements to be inserted. If there are lists with lists or similar cases, they will be merged into an one-dimensional list, and each element of this list will be inserted subsequently.
+            
+        After merging *args into an one-dimensional list, the method will verify if each item is a supported type: if not, a TypeError will be raised.
+        Then, BTree.find() will be called to see if the element is already in the BTree object. If it is, a message will be printed and the loop will move to the next item. Otherwise, Page.insert() will be called to insert the element into the closest page BTree.find() returned.
+        If a DegreeOverflowError is raised by Page.insert(), BTree.promote() will be called with the page that needed measures to be taken as a parameter.
+        """
         arguments = merge_to_list(args)
         for arg in arguments:
             if type(arg) in SUPPORTED_TYPES:
@@ -42,6 +64,15 @@ class BTree:
             self.num_keys += 1
 
     def promote(self, page):
+        """Promotes a number one level above.
+        
+        Parameters
+        ----------
+        self: BTree
+            A BTree object.
+        page: Page
+            A Page object containing the elements to be treated.
+        """
         middle_index = page.min_num_keys  # int((len(page.keys) - 1) / 2)
         left_child = page.keys[:middle_index].copy()
         right_child = page.keys[middle_index + 1 :].copy()
@@ -60,6 +91,23 @@ class BTree:
         pass
 
     def find(self, element):
+        """Finds an element in the BTree object.
+        
+        Parameters
+        ----------
+        self: BTree
+            A BTree object.
+        element: Object
+            An object to be found in the BTree.
+        
+        Returns
+        -------
+        tuple: (in_tree, page_pointer)
+            in_tree: bool
+                Tells if the item is in the BTree object.
+            page_pointer: Page
+                If in_tree == True, tells the page in which the element to be found is. Otherwise, tells the page in which the element was expected to be found.
+        """
         page_pointer = self.root
 
         while True:
@@ -73,20 +121,25 @@ class BTree:
                     return (False, page_pointer)
 
     def remove(self, *args):
+        """Removes any amount of items of the BTree object.
+        
+        Parameters
+        ----------
+        self: BTree
+            A BTree object.
+        *args: list
+            The elements to be removed. If there are lists with lists or similar cases, they will be merged into an one-dimensional list, and each element of this list will be inserted subsequently.
+            
+        After merging *args into an one-dimensional list, BTree.find() will be called to see if the element is already in the BTree object. If it is not, an ValueError will be raised. Otherwise, Page.remove() will be called to remove the element of the page BTree.find() returned.
+        If a DegreeUnderflowError is raised by Page.remove(), BTree.demote() will be called with the page that needed measures to be taken as a parameter.
+        """
         arguments = merge_to_list(args)
         for arg in arguments:
-            if type(arg) in SUPPORTED_TYPES:
-                in_tree, page_pointer = self.find(arg)
-                if in_tree == True:
-                    try:
-                        page_pointer.remove(arg)
-                    except DegreeUnderflowError as e:
-                        raise NotImplementedError()
-                else:
-                    raise ValueError("The value {} is not in this tree.".format(arg))
+            in_tree, page_pointer = self.find(arg)
+            if in_tree == True:
+                try:
+                    page_pointer.remove(arg)
+                except DegreeUnderflowError as e:
+                    raise NotImplementedError()
             else:
-                raise TypeError(
-                    "This type is not supported. Type of the argument: {}".format(
-                        str(type(arg))
-                    )
-                )
+                raise ValueError("The value {} is not in this tree.".format(arg))
