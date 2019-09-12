72f79bc4f1558e577082f1b3ec943d4533e2025c
Igor Matheus Souza Moreira <imatheus.sm@gmail.com>
2019-09-12 20:36:52 -0300

    Update playground.py
    It contains parts of B-Trees taken from Professor Reginaldo's presentation slides. They were fundamental during the testing phase!

--- playground.py
+++ playground.py
@@ -4,8 +4,16 @@ from sys import path as sys_path
 sys_path.append(getcwd())
 from btree import *
 
-# tree = BTree(2, [1, 3, 2, 4, 5])
-tree = BTree(
-    2, [3, 4, 8, 9, 10, 11, 13, 17, 20, 25, 28, 30, 33, 36, 40, 43, 45, 48, 50, 52, 55]
-)
+# tree = BTree(2, [1, 7, 14, 20, 80])
+# tree.insert(2,3,4)
+tree = BTree(2, 20, 30, 37, 50)
+tree.insert(42)
+tree.insert(47, 41)
+tree.insert(60)
+tree.insert(31, 32, 43, 44, 61, 62)
+tree.insert(33, 45)
+# tree.insert(63)
+# tree = BTree(
+#    2, [3, 4, 8, 9, 10, 11, 13, 17, 20, 25, 28, 30, 33, 36, 40, 43, 45, 48, 50, 52, 55]
+#)
 
