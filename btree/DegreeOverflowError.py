b0bbbdca16a9db4ffed283837b61ea6673e36dae
Igor Matheus Souza Moreira <imatheus.sm@gmail.com>
2019-09-13 10:39:03 -0300

    Debugging print added
    It is helping me with the promote() methods.

--- btree/DegreeOverflowError.py
+++ btree/DegreeOverflowError.py
@@ -13,3 +13,8 @@ class DegreeOverflowError(Exception):
         super().__init__(message)
         self.page = page
         self.tree = page.parent_tree
+        
+        print("[DegreeOverflowError.__init__()] Degree overflow!")
+        print("[DegreeOverflowError.__init__()] page: {}".format(page))
+        try: print("[DegreeOverflowError.__init__()] page.parent_page: {}".format(page.parent_page))
+        except: pass
