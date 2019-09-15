bf172dc5d918c37084c48a6a1c4d3916e9728dc2
Igor Matheus Souza Moreira <imatheus.sm@gmail.com>
2019-09-15 20:47:39 -0300

    RootPage and LeafPage classes deleted
    Their desired behavior is now performed by the Page class.
    
    I honestly believe that they could be of importance if well used, but in my experience throughout the development of this B-Tree project, they were just getting in the way.

--- btree/LeafPage.py
+++
@@ -1,8 +0,0 @@
-from btree.Page import *
-
-
-class LeafPage(Page):
-    """The root of the B-Tree."""
-
-    def __init__(self, min_num_keys, parent_tree=None, parent_page=None):
-        super().__init__(min_num_keys, parent_tree, parent_page)
