c573bc471d06488d970ffeb5a33950a64c839ffe
Gabriela Maximino <39780413+gabrielamaximino@users.noreply.github.com>
2019-09-23 21:58:40 -0300

    Documentação+correção
    Documentação adicionada para a lista de adjacência (ainda incompleta). Agora a lista de vértices também funciona com inteiros.

--- Graphs/AdjacencyList/graph.py
+++ Graphs/AdjacencyList/graph.py
@@ -1,12 +1,61 @@
 from AdjacencyList.node import Node
 
 class AdjListGraph:
+    """The Adjacency List Graph object."""
+
     def __init__(self, nodes, type):
+        """The Adjacency List Graph class constructor.
+
+        Parameters
+        ----------
+
+        self : AdjListGraph
+            An Adjacency List Graph object.
+
+        nodes : list 
+            The list containing graph nodes.
+
+        type : str
+            The direction type of the graph. It can be 'n-direcionado' or 'direcionado'.
+
+        Returns
+        -------
+
+        AdjListGraph
+
+            An Adjacency List Graph object containing a dictionary of Node objects, a type and an initial number of edges (0).
+
+        Methodology
+        -----------
+
+        This constructor initializes the AdjListGraph object.
+
+        """
         self.nodes = {node: Node(node) for node in nodes}
         self.type = type
         self.nEdges = 0
 
     def addEdge(self, source, destination):
+        """Adds an edge between two nodes to the graph.
+
+        Parameters
+        ----------
+
+        self : AdjListGraph
+            An Adjacency List Graph object.
+
+        source : int or str 
+            The origin node of the edge.
+
+        destination : int or str
+            The destination node of the edge.
+
+        Methodology
+        -----------
+
+        This method adds an edge between the given nodes. If type is 'n-direcionado', a double way edge is added. If not, a one-way edge is added.
+
+        """
         try:
             if self.nodes[source].isAdj(destination):
                 print("Edge already exists.")
@@ -27,6 +76,26 @@ class AdjListGraph:
             print("This node doesn't exist.")
 
     def removeEdge(self, source, destination):
+        """Removes the edge between two nodes from the graph.
+
+        Parameters
+        ----------
+
+        self : AdjListGraph
+            An Adjacency List Graph object.
+
+        source : int or str 
+            The origin node of the edge.
+
+        destination : int or str
+            The destination node of the edge.
+
+        Methodology
+        -----------
+
+        This method removes the edge between the given nodes. If edge or node don't exist, an error occurs. If not, the edge is removed.
+
+        """
         try:
             self.nodes[source].removeAdj(destination)
             
@@ -43,6 +112,26 @@ class AdjListGraph:
             print("How do you want to remove something that does not even exist? lol")
 
     def existsEdge(self, source, destination):
+        """Checks if there's an edge between two given nodes.
+
+        Parameters
+        ----------
+
+        self : AdjListGraph
+            An Adjacency List Graph object.
+
+        source : int or str 
+            The origin node of the edge.
+
+        destination : int or str
+            The destination node of the edge.
+
+        Methodology
+        -----------
+
+        This method checks the adjacent nodes list of the origin node to see if destination node is there.
+
+        """
         try:
             if self.nodes[source].isAdj(destination):
                 print("An edge was found :)")
@@ -54,6 +143,24 @@ class AdjListGraph:
             print("Oh-oh. You must provide existing nodes.")
 
     def getAdjacents(self, node):
+        """Get the adjacent nodes list of the given node.
+
+        Parameters
+        ----------
+
+        self : AdjListGraph
+            An Adjacency List Graph object.
+
+        node : int or str 
+            The identification value of the node wanted to be checked.
+
+
+        Methodology
+        -----------
+
+        This method uses the Node object method getAdjacents() to get the adjacent nodes list of the given node and print it. If the given node doesn't exist, an error occurs.
+
+        """
         try:
             print(node, "->", self.nodes[node].getAdjacents())
             
@@ -61,16 +168,75 @@ class AdjListGraph:
             print("Oh-oh. You must provide an existing node.")
 
     def showGraph(self):
+        """Prints Graph with the respective representation (Adjancency List).
+
+        Parameters
+        ----------
+
+        self : AdjListGraph
+            An Adjacency List Graph object.
+
+        Methodology
+        -----------
+
+        For each node, this method calls the Node object method getAdjacents() to get the adjacent nodes list of the respective node and print it.
+
+        """
         for node in self.nodes:
-            print(node + " -> " + self.nodes[node].getAdjacents())    
+            print(node, "->", self.nodes[node].getAdjacents())    
 
     def edgesNumber(self):
+        """Provides graph's number of edges.
+
+        Parameters
+        ----------
+
+        self : AdjListGraph
+            An Adjacency List Graph object.
+
+        Methodology
+        -----------
+
+        This method uses self.nEdges attribute to provide the number of edges of the graph. If type is 'n-direcionado', the number of edges duplicates.
+
+        """
         print("Number of edges:", self.nEdges*2 if self.type == "n-direcionado" else self.nEdges)
 
     def nodesNumber(self):
-        print("Number of nodes: ", len(self.nodes))
+        """Provides graph's number of nodes.
+
+        Parameters
+        ----------
+
+        self : AdjListGraph
+            An Adjacency List Graph object.
+
+        Methodology
+        -----------
+
+        This method calculates the length of the dictionary self.nodes, which is the number of nodes of the graph.
+
+        """
+        print("Number of nodes:", len(self.nodes))
 
     def nodeDegree(self, node):
+        """Provides the degree of a given node.
+
+        Parameters
+        ----------
+
+        self : AdjListGraph
+            An Adjacency List Graph object.
+
+        node : int or str 
+            The identification value of the node wanted to be checked.
+
+        Methodology
+        -----------
+
+        -- continuar -- 
+
+        """
         try:
             n = 0
             
@@ -79,9 +245,9 @@ class AdjListGraph:
                     n += 1
 
             if self.type == "n-direcionado":
-                print("Grau: ", n)
+                print("Degree:", n)
             else:        
-                print("Grau: ", n+len(self.nodes[node].adjacents))
+                print("Degree:", n+len(self.nodes[node].adjacents))
                 print("In-Degree:", n)
                 print("Out-Degree:", len(self.nodes[node].adjacents))
             
--- Graphs/AdjacencyList/node.py
+++ Graphs/AdjacencyList/node.py
@@ -1,19 +1,142 @@
 class Node:
+    """The Node object."""
+
     def __init__(self, value):
+        """The Node class constructor.
+
+        Parameters
+        ----------
+
+        self : Node
+            A Node object.
+
+        value : int or str
+            The value used to identify the Node.
+
+        Returns
+        -------
+
+        Node
+
+            A Node object containing the identification value and an empty start list of adjacent nodes.
+
+        Methodology
+        -----------
+
+        This constructor initializes the Node object.
+
+        """
         self.value = value
         self.adjacents = []
 
     def __str__(self):
+        """Node representation.
+
+        Parameters
+        ----------
+
+        self : Node
+            A Node object.
+
+        Returns
+        -------
+
+        str
+
+            The identification value of the Node.
+
+        Methodology
+        -----------
+
+        This method represents the Node based on its value.
+
+        """
         return self.value 
 
     def getAdjacents(self):
-            return ', '.join(x for x in self.adjacents)
+        """Get adjacent nodes of the respective Node object.
+
+        Parameters
+        ----------
+
+        self : Node
+            A Node object.
+
+        Returns
+        -------
+
+        str
+
+            Adjacent nodes of the respective Node joined by join() method.
+
+        Methodology
+        -----------
+
+        This method converts each item in the adjacent nodes list to string in order to use the join() method to join them.
+
+        """
+        return ', '.join(str(x) for x in self.adjacents)
 
     def addAdj(self, node):
+        """Adds an adjacent node to the respective Node.
+
+        Parameters
+        ----------
+
+        self : Node
+            A Node object.
+
+        node : int or str
+            The identification value of the adjacent Node to be added. 
+
+        Methodology
+        -----------
+
+        This method adds the given node to the adjacent nodes list of the respective node.
+        """
         self.adjacents.append(node)
 
     def removeAdj(self, node):
+        """Removes an adjacent node from the respective Node.
+
+        Parameters
+        ----------
+
+        self : Node
+            A Node object.
+
+        node : int or str
+            The identification value of the adjacent Node to be removed. 
+
+        Methodology
+        -----------
+
+        This method removes the given node from the adjacent nodes list of the respective node.
+        """
         self.adjacents.remove(node)
 
     def isAdj(self, node):
+        """Check if a given node is adjacent of the respective Node.
+
+        Parameters
+        ----------
+
+        self : Node
+            A Node object.
+
+        node : int or str
+            The identification value of the Node to be checked. 
+
+        Returns
+        -----------
+
+        bool 
+            Whether the given node is part of adjacent nodes list or not.
+
+
+        Methodology
+        -----------
+
+        This method checks if the given node is in the adjacent nodes list of the respective Node.
+        """
         return True if node in self.adjacents else False
