ab9a853471d0498bd3f2ba175fa3325ce8544540
Igor Matheus Souza Moreira <imatheus.sm@gmail.com>
2019-10-18 09:17:24 -0300

    AdjMatrixGraph.__repr__() improved
    Now, for every number of nodes, the values are properly aligned.

--- Graphs/AdjacencyMatrix/graph.py
+++ Graphs/AdjacencyMatrix/graph.py
@@ -306,10 +306,10 @@ class AdjMatrixGraph:
         """
         max_length = len(str(self.nNodes))
         representation = "<AdjMatrixGraph object>\n"
-        representation += (max_length + 3) * " " + " ".join([str(x).ljust(max_length, " ") for x in range(len(self))]) + "\n\n"
+        representation += (max_length + 2) * " " + " ".join([str(x).ljust(max_length, " ") for x in range(len(self))]) + "\n\n"
         
         for i, x in enumerate(self):
-            representation += str(i).ljust(max_length + 2, " ") + " ".join([str(int(y)).rjust(max_length, " ") for y in x]) + "\n"
+            representation += str(i).ljust(max_length + 2, " ") + " ".join([str(int(y)).ljust(max_length, " ") for y in x]) + "\n"
         
         return representation[:-1]
 
