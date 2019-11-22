124f4dde8c280b8097d2c705c9cb4e12951286bb
Igor Matheus Souza Moreira <imatheus.sm@gmail.com>
2019-11-22 09:59:23 -0300

    handle_txt() now handles encoding as well
    Punctuation problems originated by the incorrect default encoding were mitigated by this commit.

--- String processing/patternMatching/helpers.py
+++ String processing/patternMatching/helpers.py
@@ -1,6 +1,6 @@
-def handle_txt(file_path):
+def handle_txt(file_name):
     try:
-        with open(f"text_examples/{file_path}", 'r') as file:
+        with open(f"text_examples/{file_name}", 'r', encoding="utf-8") as file:
             return ''.join(file.readlines())
     except FileNotFoundError:
-        raise FileNotFoundError(f"No such file or directory has been found: text_examples/{file_path}")
+        raise FileNotFoundError(f"No such file or directory has been found: text_examples/{file_name}")
